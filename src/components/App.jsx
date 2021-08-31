c6d281c0ad4a0f086886d39a1110617395702677
Александр <64688166+varynskyi@users.noreply.github.com>
2021-08-31 22:10:50 +0300

    fix
    add lint-staged

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,15 +9,18 @@ import transactions from './TransactionHistory/transactions.json';
 
 export default function App() {
   return <div>
-   <Profile
-  name={user.name}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-    />
-    <Statistics title="Upload stats" stats={stats} />
-    <FriendList friends={friends} />
-    <TransactionHistory items={transactions} />
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      />
+    <Statistics title="Upload stats"
+      stats={stats} />
+    <FriendList
+      friends={friends} />
+    <TransactionHistory
+      items={transactions} />
   </div>;
 }
